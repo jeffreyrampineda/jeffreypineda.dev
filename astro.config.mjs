6192e0bc226b8bc7e96a200e10fe635901a4b326
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2025-04-14 15:36:53 -0400

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jeffreypineda.dev',
   integrations: [mdx(), sitemap(), tailwind()],
 });
